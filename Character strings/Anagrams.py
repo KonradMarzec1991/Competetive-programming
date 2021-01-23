6adbcf54a8a86d57065dfce44954c740506967de
KonradMarzec1991 <konrimarzec@gmail.com>
2021-01-23 14:05:21 +0100

    Text T9 keys

--- Character strings/Anagrams.py
+++ Character strings/Anagrams.py
@@ -1,11 +1,3 @@
-"""
-output: {bowel below elbow}, {arc car}, {night thing}, {cried cider}, {act cat}
-"""
-
-text = """below the car is a rat drinking cider and bending its elbow while this thing
-is an arc that can act like a cat which cried during the night caused by pain in its
-bowel"""
-
 import re
 
 RE_PATTERN = re.compile(r'\w+')
@@ -23,4 +15,6 @@ def anagrams(S):
     return [sorted(list(arr)) for arr in d.values() if len(arr) > 1]
 
 
-print(anagrams(text))
+print(anagrams("""below the car is a rat drinking cider and bending its elbow while this thing
+is an arc that can act like a cat which cried during the night caused by pain in its
+bowel"""))
